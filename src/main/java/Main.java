9c6673fa9d6d32d71ee210ca29e5fbb90020b7f3
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-05-27 11:03:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Cálculo da velocidade média em km/h");
+    System.out.println();
+    System.out.println("A distância considerada é");
+    int distancia = 475;
+    System.out.println(distância);
+    System.out.println();
+    System.out.println("o tempo considerado é ")
+    int tempo = 100s
+      
+  }
+
+  
+   
+}
